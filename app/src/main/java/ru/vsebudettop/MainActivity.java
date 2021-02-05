4199b26ed6f0046a7a7255e7529af2dcc8094228
vasia18 <studiovasia18@gmail.com>
2021-02-05 20:48:48 +0300

    Добавил файл dimens.xml

--- app/src/main/java/ru/vsebudettop/MainActivity.java
+++ app/src/main/java/ru/vsebudettop/MainActivity.java
@@ -2,7 +2,10 @@ package ru.vsebudettop;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,10 @@ public class MainActivity extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+	}
+
+
+	public void Btn_Start_2(View view){
+
 	}
 }
