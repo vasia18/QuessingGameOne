e6935c4b7d1e8f71ee04702f0fc77438f75a20f2
vasia <44756547+vasia18@users.noreply.github.com>
2021-02-05 22:51:29 +0300

    Добавил в AndroidManifest.xml код который недает менять ориентацию приложения Window window = getWindow();         window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
        }

--- app/src/main/java/ru/vsebudettop/MainActivity.java
+++ app/src/main/java/ru/vsebudettop/MainActivity.java
@@ -1,22 +1,27 @@
 package ru.vsebudettop;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Убрать строку состояния на телефоне
+        Window window = getWindow();
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+    }
 
 
-	public void Btn_Start_2(View view){
+    public void Btn_Start_2(View view) {
 
-	}
+    }
 }
