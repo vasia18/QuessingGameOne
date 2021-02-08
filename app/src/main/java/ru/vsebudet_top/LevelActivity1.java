94779f73717c97a578c9830f4af841302026efe4
vasia <44756547+vasia18@users.noreply.github.com>
2021-02-08 22:59:21 +0300

    Добавлено новое активити universal.xml

---
+++ app/src/main/java/ru/vsebudet_top/LevelActivity1.java
@@ -0,0 +1,15 @@
+package ru.vsebudet_top;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+class LevelActivity1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.universal);
+    }
+}
