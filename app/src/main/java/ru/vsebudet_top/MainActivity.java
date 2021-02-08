44f64fda613f575c96bb5039062db43e6af86f64
vasia18 <studiovasia18@gmail.com>
2021-02-08 09:51:36 +0300

    добавлен переход c MainActivity.java на GameLevelActvity.java

--- app/src/main/java/ru/vsebudet_top/MainActivity.java
+++ app/src/main/java/ru/vsebudet_top/MainActivity.java
@@ -14,12 +14,13 @@ public class MainActivity extends AppCompatActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		Button button_start = findViewById(R.id.button_start);
+		setContentView(R.layout.activity_main); //Подключаем activity_main к MainActivity.java
+		Button button_start = findViewById(R.id.button_start); // Находим кнопку, которая при нажатии будет переходить на другое Activity
 		button_start.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
 				try {
+					// Прописываем с какого Activity на какое Activity будет переходить при нажатии на кнопку
 					Intent intent = new Intent(MainActivity.this,
 							GameLevelActvity.class);
 					startActivity(intent);
