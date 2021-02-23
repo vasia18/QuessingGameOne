7bb9ed6899abd87700abcbf093fb093701b829bb
vasia18 <studiovasia18@gmail.com>
2021-02-23 20:42:34 +0300

    Сделано: папка anim top_animation

--- app/src/main/java/ru/vsebudet_top/SplashScreen.java
+++ app/src/main/java/ru/vsebudet_top/SplashScreen.java
@@ -2,19 +2,29 @@ package ru.vsebudet_top;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.SystemClock;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
 import java.util.concurrent.TimeUnit;
 
 public class SplashScreen extends AppCompatActivity {
+
+	Animation top_animation;  // прописываем
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
 		SystemClock.sleep(TimeUnit.SECONDS.toMillis(3));  // ставим таймер на Splash screen
+
+		//анимация
+		Context context;
+		top_animation = AnimationUtils.loadAnimation(this, R.anim.top_animation);
+
 
 		Intent intent = new Intent(this, MainActivity.class); //обьявляем намеренья перехода из тикущего окна (SplashScreen.java) в окно MainActivity.java
 		startActivity(intent); //запускаем намеренья
