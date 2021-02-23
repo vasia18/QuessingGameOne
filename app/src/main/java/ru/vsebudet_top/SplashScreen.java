61e7e1c929e6dc397ec2823ddf659ee0e473b88d
vasia18 <studiovasia18@gmail.com>
2021-02-23 16:20:07 +0300

    Сделано: Установил таймер (SystemClock.sleep) в plash_Screen

--- app/src/main/java/ru/vsebudet_top/SplashScreen.java
+++ app/src/main/java/ru/vsebudet_top/SplashScreen.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.SystemClock;
+
+import java.util.concurrent.TimeUnit;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -11,8 +14,10 @@ public class SplashScreen extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
-		Intent intent = new Intent(this, MainActivity.class);
-		startActivity(intent);
-		finish();
+		SystemClock.sleep(TimeUnit.SECONDS.toMillis(3));  // ставим таймер на Splash screen
+
+		Intent intent = new Intent(this, MainActivity.class); //обьявляем намеренья перехода из тикущего окна (SplashScreen.java) в окно MainActivity.java
+		startActivity(intent); //запускаем намеренья
+		finish(); //закрыть
 	}
 }
