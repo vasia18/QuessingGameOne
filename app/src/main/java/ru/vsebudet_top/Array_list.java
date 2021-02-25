d612cf05268f02588f09415595ac1548a4128605
vasia18 <studiovasia18@gmail.com>
2021-02-25 12:15:41 +0300

    Сделано: 1

---
+++ app/src/main/java/ru/vsebudet_top/Array_list.java
@@ -0,0 +1,30 @@
+package ru.vsebudet_top;
+
+public class Array_list {
+
+	//масив для первого уровня - начало
+	final int[] images_level_1 = {
+			R.drawable.onelevel_1,
+			R.drawable.onelevel_2,
+			R.drawable.onelevel_3,
+			R.drawable.onelevel_4,
+			R.drawable.onelevel_5,
+			R.drawable.onelevel_6,
+			R.drawable.onelevel_7,
+			R.drawable.onelevel_8,
+			R.drawable.onelevel_9,
+			R.drawable.onelevel_10
+	};
+
+	//код для нового масива с цыфрами - начало
+	final int[] power_1 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+	//код для текста
+	final String[] text_1 = {
+			R.string.level1_text1,
+
+	};
+
+
+	//масив для первого уровня - конец
+}
